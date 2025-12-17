aa09a6604883b7fdd4f356a5ad1108d9409def99
Xin Li <happyxin331@icloud.com>
2025-12-17 12:58:23 -0800

    solve it

--- pairings.py
+++ pairings.py
@@ -1,8 +1,19 @@
 def find_pairs(num_string):
   # Write your solution here!
-  pass
+  #convert the string to a list of number
+  #initiate an empty set
+  #loop through the list of number , the outer loop will loop from index 0 to the last but one element
+  #nested loop will iterate through the index 1 to the last element
+  # we will add those two numbers to a tuple and sort it and add it to an emtpy set
+  #when the loop is done, return this set
 
-
+  nums= num_string.split()
+  result = set()
+  for i in range(len(nums)-1): 
+    for j in range(i+1,len(nums)):
+      ordered = sorted((int(nums[i]),int(nums[j])))
+      result.add(tuple(ordered))
+  return result
 # Test cases
 assert find_pairs("7 3 99") == {(7, 99), (3, 7), (3, 99)}
 assert find_pairs("2 1") == {(1, 2)}
